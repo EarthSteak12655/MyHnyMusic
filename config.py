82036f0dc9722a0f87637dcf2f4051266cd247f7
EarthSteak12655 <100482626+EarthSteak12655@users.noreply.github.com>
2022-08-11 13:54:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,16 @@ load_dotenv()
 get_queue = {}
 
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "14447155"))
+API_HASH = getenv("API_HASH","ef370b85866716be28c348f5f96db1e4")
 
 ASS_HANDLER = list(getenv("ASS_HANDLER", ".").split())
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN","5536017329:AAGccrIGEo95SLLa19fYfrxNmB0DfIcWQdk")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
-LOGGER_ID = int(getenv("LOGGER_ID"))
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+LOGGER_ID = int(getenv("LOGGER_ID","-1001584910654"))
+MONGO_DB_URI = getenv("MONGO_DB_URI","")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5442143084").split()))
 
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/74fb72c6e6b04b6fc5eef.jpg")
 START_IMG = getenv("START_IMG","https://te.legra.ph/file/ec2b6bf8d17962430bc7e.jpg")
@@ -23,5 +23,5 @@ START_IMG = getenv("START_IMG","https://te.legra.ph/file/ec2b6bf8d17962430bc7e.j
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TeamTrickyYash")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/TrickyYash")
 
-STRING_SESSION = getenv("STRING_SESSION", None)
+STRING_SESSION = getenv("STRING_SESSION", "")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1574818111").split()))
